ccbac9051388a1d701d3bed82324d4c8b245b792
Adrien Patuzzo <a.pats@hotmail.fr>
2023-09-21 20:13:39 +0200

    planty

--- app/public/wp-content/themes/planty/functions.php
+++ app/public/wp-content/themes/planty/functions.php
@@ -28,3 +28,13 @@ function my_theme_enqueue_styles()
 		$theme->get('Version') // This only works if you have Version defined in the style header.
 	);
 }
+
+function add_admin_link_to_menu($items, $args)
+{
+	if (is_user_logged_in() && current_user_can('administrator') && $args->theme_location == 'primary') {
+		$admin_url = admin_url();
+		$items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-47"><div class="wrap"><a href="' . esc_url($admin_url) . '">Admin</a></div></li>';
+	}
+	return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2);
